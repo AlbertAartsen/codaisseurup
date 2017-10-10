8e392664ca27e05f8294fd2f23ce359d747d6419
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-10 11:37:20 +0200

    event model & one-to-many

---
+++ app/models/event.rb
@@ -0,0 +1,16 @@
+class Event < ApplicationRecord
+belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :description, presence: true, length: { maximum: 400 }
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :capacity, presence: true
+  validates :includes_food, presence: true
+  validates :includes_drinks, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates :active, presence: true
+
+
+end
