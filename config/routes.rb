89dd7d774a5c7fe394dc2d899efe48f6fba42d65
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-11 22:03:21 +0200

    add about page test

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ root to: 'pages#home'
   resources :users, only: [:show]
   resources :events, except: [:destroy]
   resources :profiles, only: [:new, :edit, :create, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "about" => "pages#about"
 end
