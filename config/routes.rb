2025577ecffb4b6c69fd827de86ce6a4b26110db
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-13 15:15:23 +0200

    through association views

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,15 @@ root to: 'pages#home'
 
   devise_for :users
   resources :users, only: [:show]
-  resources :events, except: [:destroy]
+
+  resources :events do
+    resources :registrations, only: [:create]
+  end
+
   resources :profiles, only: [:new, :edit, :create, :update]
   resources :photos
+
+
 
   get "about" => "pages#about"
 end
